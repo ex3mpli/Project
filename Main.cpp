f6bd941850430465d72e076fd068a002cb82c5c4
Earl <ex3mpli@users.noreply.github.com>
2016-10-08 02:26:58 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,8 @@
 #define DllExport   __declspec(dllexport)
 #define WallArray   0x467F74
 
-int wall, cghost = 0;
+
+int wall = 0; int cghost = 0; int wm4a1 = 1;
 
 bool Game()
 {
@@ -15,8 +16,18 @@ bool Game()
 
 void Exempli(void)
 {
-  DWORD cshell = (DWORD)GetModuleHandleW(L"CShell.dll");
-  DWORD Wall = *(DWORD*)(WallArray + 0xA);
+  DWORD cshell  = (DWORD)GetModuleHandleW(L"CShell.dll");
+  DWORD m4a1    = *(DWORD*)((*(DWORD*)(CShell+WeaponMgr))+(4*11));
+  DWORD m4a1v   = *(DWORD*)((*(DWORD*)(CShell+WeaponMgr))+(4*856));
+  DWORD Wall    = *(DWORD*)(WallArray + 0xA);
+  
+  if(wm4a1)
+  {
+    for(int i = 1; i < 1802; ++i)
+    {
+      *(DWORD*)(m4a1 + i) = *(DWORD)(m4a1v + i);
+    }
+  }
   
   if(GetAsyncKeyState(VK_F2) &1) wall=!wall;
   if(wall)
@@ -46,8 +57,8 @@ extern "C" DllExport BOOLEAN APIENTRY DllMain( IN HINSTANCE hDllHandle, IN DWORD
   switch( fwdReason )
   {
     case DLL_PROCESS_ATTACH:
-      DisableThreadLibraryCalls( hDllHandle );
-	  MessageBoxA(NULL,"RAML \n\tPUBLIC HACK", "Made by: Chain", MB_OK | MB_ICONASTERISK );
+    DisableThreadLibraryCalls( hDllHandle );
+	  MessageBoxA(NULL,"RAML\n\tPUBLIC HACK", "Made by: Chain",NULL MB_OK);
 	  CreateThread(NULL, NULL,(LPTHREAD_START_ROUTINE)start, NULL, NULL, NULL);
     break;
   }
