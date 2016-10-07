dda9c93a73f7b29a2317b1c33680fc076f5e7aeb
Earl <ex3mpli@users.noreply.github.com>
2016-10-07 20:36:22 +0800

    Create class.h

---
+++ class.h
@@ -0,0 +1,15 @@
+#ifndef my_project
+#define my_project
+
+class project
+{
+  public:
+    BOOL myProject();
+    void main(void);
+    void func(void);
+    
+    int ady = true;
+    int dy = true;
+}
+
+#endif my_project
