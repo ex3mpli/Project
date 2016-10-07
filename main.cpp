f97c5a7ff973f3ec068969a3637f22e5d67b5a6f
Earl <ex3mpli@users.noreply.github.com>
2016-10-07 22:22:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,25 @@
 #include <windows.h>
-#include <process.h>
-#include "class.h"
 
-#define DllExport   __declspec(dllexport)
-#define address   0x467F74
-
-BOOL project::myProject(DWORD cshell, DWORD clientfx)
+bool client(DWORD cshell, DWORD clientfx)
 {
-  cshell = (GetModulehandleW(L"CShell.dll") != NULL);
-  clientfx = (GetModuleHandleW(L"ClientFx.fxd") != NULL);
-  return true;
-  return fasle;
+  BOOLEAN success = true; BOOLEAN failed = false;
+  cshell    = (GetModuleHandleW(L"CShell.dll") != NULL);
+  clientfx  = (GetModuleHandleW(L"Clientfx.fxd") != NULL);
+  return  sucess;
+  return  failed;
 }
 
-void project::main(void)
+void main(DWORD cshell)
 {
-  DWORD adres = *(DWORD)(address + 10);
+  cshell = GetModuleHandleW(L"CShell.dll");
+}
+
+void start()
+{
   
-  if(GetAsyncKeyState(VK_F2) &1) ady = !ady;
-  if(ady)
-    memcpy((void*)(adres + 164), "\x01\x01\x01\x01", 4);
-  else
-    memcpy((void*)(adres + 164), "\x00\x00\x00\x00", 4);
-
-  if(GetAsyncKeyState(VK_F2) &1) dy = !dy;
-  if(dy)
-    *(DWORD*)(adres + 184) = 5;
-  else
-    *(DWORD*)(adres + 184) = 14;
 }
 
-void project::func(void)
-{
-  while(!this->myProject())
-  Sleep(200);
-  while(true)
-  {
-    this->main();
-  }
-}
-
-extern "C" DllExport
-BOOL WINAPI DllMain( IN HINSTANCE hDllHandle, IN DWORD fwdReason, IN LPVOID lpReserved )
+BOOLEAN APIENTRY DllMain( IN HINSTANCE hDllHandle, IN DWORD fwdReason, IN LPVOID lpReserved )
 {
   BOOLEAN bSuccess = TRUE;
   
@@ -49,16 +27,11 @@ BOOL WINAPI DllMain( IN HINSTANCE hDllHandle, IN DWORD fwdReason, IN LPVOID lpRe
   {
     case DLL_PROCESS_ATTACH:
     DisableThreadLibraryCalls ( hDllHandle );
-    _beginthread(NULL,NULL,&this->func,NULL,NULL);
+    _beginthread(NULL,NULL,&start,NULL,NULL,NULL);
     break;
-    
-    case DLL_PROCESS_DETACH:
-    breack;
-    
     case DLL_THREAD_ATTACH:
-    breack;
-    
     case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
     break;
   }
   return bSuccess;
