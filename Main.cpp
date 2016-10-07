662dd99dce33b6a56482cc0f011dfcffd866606e
Earl <ex3mpli@users.noreply.github.com>
2016-10-08 02:37:04 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,9 @@
 
 #define DllExport   __declspec(dllexport)
 #define WallArray   0x467F74
-
+#define WeaponMgr   0x01A16B2C
+#define Bypass28_3  
+#define Bypass31_0  
 
 int wall = 0; int cghost = 0; int wm4a1 = 1;
 
@@ -17,9 +19,22 @@ bool Game()
 void Exempli(void)
 {
   DWORD cshell  = (DWORD)GetModuleHandleW(L"CShell.dll");
+  DWORD pWepMgr = *(DWORD*)(CShell+WeaponMgr);
+  DWORD bypass8 = DWORD(CShell + Bypass28_3);
+  DWORD bypass3 = DWORD(CShell + Bypass31_0);
   DWORD m4a1    = *(DWORD*)((*(DWORD*)(CShell+WeaponMgr))+(4*11));
   DWORD m4a1v   = *(DWORD*)((*(DWORD*)(CShell+WeaponMgr))+(4*856));
   DWORD Wall    = *(DWORD*)(WallArray + 0xA);
+  
+  if(bypass8!=NULL)
+  {
+    memcpy((void*)(bypass8), "\xE9\xe3\x01\x00\x00\x90", 6)
+  }
+  
+  if(bypass3!=NULL)
+  {
+    memcpy((void*)(bypass3), "\xEB\x48",2)
+  }
   
   if(wm4a1)
   {
