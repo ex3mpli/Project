b98cfff7c7cc587e8ad52e5ac6983923efdab640
Earl <ex3mpli@users.noreply.github.com>
2016-10-08 02:58:14 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,11 @@
 #define DllExport   __declspec(dllexport)
 #define WallArray   0x467F74
 #define WeaponMgr   0x01A16B2C
-#define Bypass28_3  
-#define Bypass31_0  
+#define Bypass28_3  0x005A1841
+#define Bypass31_0  0x005A2285
 
 int wall = 0; int cghost = 0; int wm4a1 = 1;
-
+int bypass
 bool Game()
 {
   if(GetModuleHandleA("CShell.dll") != NULL && GetModuleHandleA("Clientfx.fxd") != NULL)
@@ -19,28 +19,28 @@ bool Game()
 void Exempli(void)
 {
   DWORD cshell  = (DWORD)GetModuleHandleW(L"CShell.dll");
-  DWORD pWepMgr = *(DWORD*)(CShell+WeaponMgr);
-  DWORD bypass8 = DWORD(CShell + Bypass28_3);
-  DWORD bypass3 = DWORD(CShell + Bypass31_0);
-  DWORD m4a1    = *(DWORD*)((*(DWORD*)(CShell+WeaponMgr))+(4*11));
-  DWORD m4a1v   = *(DWORD*)((*(DWORD*)(CShell+WeaponMgr))+(4*856));
+  DWORD pWepMgr = *(DWORD*)(cshell+WeaponMgr);
+  DWORD bypass8 = DWORD(cshell + Bypass28_3);
+  DWORD bypass3 = DWORD(cshell + Bypass31_0);
+  DWORD m4a1    = *(DWORD*)((*(DWORD*)(cshell+WeaponMgr))+(4*11));
+  DWORD m4a1v   = *(DWORD*)((*(DWORD*)(cshell+WeaponMgr))+(4*856));
   DWORD Wall    = *(DWORD*)(WallArray + 0xA);
   
   if(bypass8!=NULL)
   {
-    memcpy((void*)(bypass8), "\xE9\xe3\x01\x00\x00\x90", 6)
+    memcpy((void*)(bypass8), "\xE9\xe3\x01\x00\x00\x90", 6);
   }
   
   if(bypass3!=NULL)
   {
-    memcpy((void*)(bypass3), "\xEB\x48",2)
+    memcpy((void*)(bypass3), "\xEB\x48",2);
   }
   
   if(wm4a1)
   {
     for(int i = 1; i < 1802; ++i)
     {
-      *(DWORD*)(m4a1 + i) = *(DWORD)(m4a1v + i);
+      *(DWORD*)(m4a1 + i) = *(DWORD*)(m4a1v + i);
     }
   }
   
@@ -73,7 +73,7 @@ extern "C" DllExport BOOLEAN APIENTRY DllMain( IN HINSTANCE hDllHandle, IN DWORD
   {
     case DLL_PROCESS_ATTACH:
     DisableThreadLibraryCalls( hDllHandle );
-	  MessageBoxA(NULL,"RAML\n\tPUBLIC HACK", "Made by: Chain",NULL MB_OK);
+	  MessageBoxA(NULL,"RAML\n\tPUBLIC HACK", "Made by: Chain", MB_OK);
 	  CreateThread(NULL, NULL,(LPTHREAD_START_ROUTINE)start, NULL, NULL, NULL);
     break;
   }
