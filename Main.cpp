8e33c81a23dce9b49cc29aa324ebfa2bece6d92e
Earl <ex3mpli@users.noreply.github.com>
2016-10-10 18:16:24 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,23 +3,22 @@
 
 #define DllExport   __declspec(dllexport)
 #define WallArray   0x467F74
-#define WeaponMgr   0x01A16B2C
+#define WeaponMgr   0x1A16B2C
 #define Bypass28_3  0x005A1841
 #define Bypass31_0  0x005A2285
 
 int wall = 0; int cghost = 0; int wm4a1 = 1;
-int bypass
-bool Game()
+
+bool Game() //we are checking if CShell.dll & Clientfx.fxd are not equal to NULL or 0 
 {
   if(GetModuleHandleA("CShell.dll") != NULL && GetModuleHandleA("Clientfx.fxd") != NULL)
   return  true;
   return  false;
 }
 
-void Exempli(void)
+void Exempli(void) //void has no parameter
 {
   DWORD cshell  = (DWORD)GetModuleHandleW(L"CShell.dll");
-  DWORD pWepMgr = *(DWORD*)(cshell+WeaponMgr);
   DWORD bypass8 = DWORD(cshell + Bypass28_3);
   DWORD bypass3 = DWORD(cshell + Bypass31_0);
   DWORD m4a1    = *(DWORD*)((*(DWORD*)(cshell+WeaponMgr))+(4*11));
@@ -46,9 +45,9 @@ void Exempli(void)
   
   if(GetAsyncKeyState(VK_F2) &1) wall=!wall;
   if(wall)
-    memcpy((void*)(Wall + 0xA4), "\x01\x01\x01\x01", 4);
-  else
     memcpy((void*)(Wall + 0xA4), "\x00\x00\x00\x00", 4);
+  else
+    memcpy((void*)(Wall + 0xA4), "\x01\x01\x01\x01", 4);
   
   if(GetAsyncKeyState(VK_F3) &1) cghost=!cghost;
   if(cghost)
@@ -57,25 +56,26 @@ void Exempli(void)
     *(DWORD*)(Wall + 0xB8) = 14;
 }
 
-void start(void)
+void start(void) //void has no parameter
 {
-  while(!Game()) Sleep(200);
-  while(1)
+  while(!Game()) Sleep(200); //while game doesn't ready sleep 200
+  while(1) //while 1 game is ready!
   {
     Exempli();
   }
 }
 
-extern "C" DllExport BOOLEAN APIENTRY DllMain( IN HINSTANCE hDllHandle, IN DWORD fwdReason, IN LPVOID lpReserved )
+extern "C" DllExport BOOLEAN APIENTRY DllMain( IN HINSTANCE Dll, IN DWORD fwdReason, IN LPVOID lpReserved )
 {
-  BOOLEAN bSuccess = TRUE;
+  BOOLEAN Success = TRUE;
   switch( fwdReason )
   {
     case DLL_PROCESS_ATTACH:
-    DisableThreadLibraryCalls( hDllHandle );
-	  MessageBoxA(NULL,"RAML\n\tPUBLIC HACK", "Made by: Chain", MB_OK);
-	  CreateThread(NULL, NULL,(LPTHREAD_START_ROUTINE)start, NULL, NULL, NULL);
+      DisableThreadLibraryCalls(Dll);
+	    MessageBoxA(NULL,"YOUR MESSAGE", "YOUR TITLE", MB_OK | MB_ICONEXCLAMATION);
+	    CreateThread(NULL, NULL,(LPTHREAD_START_ROUTINE)start, NULL, NULL, NULL);
+      /* _beginthreadex(NULL,NULL,start,NULL,NULL,NULL); */ //required header process.h
     break;
   }
-  return bSuccess;
+  return Success;
 }
