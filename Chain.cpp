164c03de92e44132fc8888cce7f8d6dcf61be007
Earl <ex3mpli@users.noreply.github.com>
2016-10-14 21:38:53 +0800

    Create Chain.cpp

---
+++ Chain.cpp
@@ -0,0 +1,72 @@
+#include <windows.h>
+
+#define DllExport   __declspec(dllexport)
+#define WallArray   0x469404  //need to update
+#define ModelNode   0x1ACB7D4 //need to update
+
+typedef struct {
+    float damage;
+    int original;
+} BACKUP;
+
+BACKUP Backup[1802];
+
+int wall = 0; int cghost = 0;
+
+bool Game() //we are checking if CShell.dll & Clientfx.fxd are not equal to NULL or 0 
+{
+  if(GetModuleHandleA("CShell.dll") != NULL && GetModuleHandleA("Clientfx.fxd") != NULL)
+  return  true; //return true if true
+  return  false; //return false if false
+}
+
+void Exempli(void) //void has no parameter/value
+{
+  DWORD cshell  = (DWORD)GetModuleHandleW(L"CShell.dll");
+  DWORD ModelN  = *(DWORD*)(cshell + Model);
+  DWORD Wall    = *(DWORD*)(WallArray + 0xA);
+  
+  if(GetAsyncKeyState(VK_F2) &1) wall=!wall;
+  if(wall)
+    memcpy((void*)(Wall + 0xA4), "\x00\x00\x00\x00", 4);
+  else
+    memcpy((void*)(Wall + 0xA4), "\x01\x01\x01\x01", 4);
+  
+  if(GetAsyncKeyState(VK_F3) &1) cghost=!cghost;
+  if(cghost)
+    *(DWORD*)(Wall + 0xB8) = 5;
+  else
+    *(DWORD*)(Wall + 0xB8) = 14;
+  
+  if(ModelN!=NULL)
+  {
+    for(int i = 0; i < 1802; i++)
+      for(int x = 0; x < 3; x++)
+      {
+        *(float*)((ModelN + 0x54 +  x*4 ) + (i*0x9c)) = 250.0f;
+      }
+  }
+}
+
+void start(void) //void has no parameter/value
+{
+  while(!Game()) Sleep(200); //while game doesn't ready sleep 200
+  while(1) //while 1 game is ready!
+  {
+    Exempli();
+  }
+}
+
+extern "C" DllExport BOOLEAN APIENTRY DllMain( IN HINSTANCE Dll, IN DWORD fwdReason, IN LPVOID lpReserved )
+{
+  BOOLEAN Success = TRUE;
+  switch( fwdReason )
+  {
+    case DLL_PROCESS_ATTACH:
+DisableThreadLibraryCalls(Dll);
+MessageBoxA(NULL,"YOUR MESSAGE", "YOUR TITLE", MB_OK | MB_ICONEXCLAMATION);
+CreateThread(NULL, NULL,(LPTHREAD_START_ROUTINE)start, NULL, NULL, NULL);
+    break;
+  }
+  return Success;
+}
